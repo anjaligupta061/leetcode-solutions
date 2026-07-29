b90374e97657f2b47cc74b9d22669820a49835e1
anjaligupta061 <anjali14082005@gmail.com>
2026-07-29 14:48:02 +0530

    Time: 0 ms (100%), Space: 12.2 MB (72.41%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.length();
+        int start = 0;
+        for (int i = 0; i <= n; i++) {
+            // Word khatam
+            if (i == n || s[i] == ' ') {
+                reverse(s.begin() + start, s.begin() + i);
+                start = i + 1;
+            }
+        }
+        return s;
+    }
+};
