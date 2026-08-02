93c7f8b4cbd47eb5f4363a5bcae9e0152af6446d
anjaligupta061 <anjali14082005@gmail.com>
2026-08-02 21:53:05 +0530

    Time: 0 ms (100%), Space: 10.3 MB (48.86%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
     string reverseVowels(string s) {
-        int n = s.length();
-        string ans;
-        deque<char> dq;
-        for(int i=0;i<n;i++){
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' ||
-            s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
-                dq.push_back(s[i]);
-            }
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !isVowel(s[left]))
+                left++;
+            while (left < right && !isVowel(s[right]))
+                right--;
+            swap(s[left], s[right]);
+            left++;
+            right--;
         }
-        reverse(dq.begin(),dq.end());
-        for(int i=0;i<n;i++){
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' ||
-            s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
-                ans+=dq.front();
-                dq.pop_front();
-            }
-            else ans+=s[i];
-        }
-        return ans;
+        return s;
     }
 };
