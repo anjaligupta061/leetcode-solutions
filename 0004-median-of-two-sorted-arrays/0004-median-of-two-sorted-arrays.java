89fe563e4989cc7face1ce6ac6c517221b35998a
anjaligupta061 <anjali14082005@gmail.com>
2026-09-04 19:35:22 +0530

    Time: 1 ms (100%), Space: 49 MB (32.25%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -0,0 +1,72 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+
+        // Binary search smaller array
+        if (n1 > n2) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int n = n1 + n2;
+
+        // Number of elements required on left side
+        int left = (n1 + n2 + 1) / 2;
+
+        int low = 0;
+        int high = n1;
+
+        while (low <= high) {
+
+            int mid1 = low + (high - low) / 2;
+            int mid2 = left - mid1;
+
+            int l1 = Integer.MIN_VALUE;
+            int l2 = Integer.MIN_VALUE;
+
+            int r1 = Integer.MAX_VALUE;
+            int r2 = Integer.MAX_VALUE;
+
+            if (mid1 < n1) {
+                r1 = nums1[mid1];
+            }
+
+            if (mid2 < n2) {
+                r2 = nums2[mid2];
+            }
+
+            if (mid1 - 1 >= 0) {
+                l1 = nums1[mid1 - 1];
+            }
+
+            if (mid2 - 1 >= 0) {
+                l2 = nums2[mid2 - 1];
+            }
+
+            // Correct partition
+            if (l1 <= r2 && l2 <= r1) {
+
+                // Odd total elements
+                if (n % 2 == 1) {
+                    return Math.max(l1, l2);
+                }
+
+                // Even total elements
+                return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+            }
+
+            // Too many elements taken from nums1
+            else if (l1 > r2) {
+                high = mid1 - 1;
+            }
+
+            // Too few elements taken from nums1
+            else {
+                low = mid1 + 1;
+            }
+        }
+
+        return 0.0;
+    }
+}
